bfa115654d76575e8f95804009647a2a3e2638ec
dharma0907 <83895329+dharma0907@users.noreply.github.com>
2024-07-28 13:03:07 +0530

    Update ekscluster.tf

--- eks-cluster/ekscluster.tf
+++ eks-cluster/ekscluster.tf
@@ -44,7 +44,7 @@ resource "aws_eks_node_group" "nodegroup_dharmaekscluster" {
 
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.small"]
+  instance_types = ["t2.small"]
 
   scaling_config {
     desired_size = 3
